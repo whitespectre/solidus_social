d01ef24d4bc4ddd85e05e80c9f014d91a46ddffb
Anton Kopylov <anton@kopylov.net>
2019-07-26 10:53:48 +0300

    New facebook callback logic

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -7,7 +7,16 @@ Spree.user_class.class_eval do
     if omniauth.fetch('info', {})['email'].present?
       self.email = omniauth['info']['email'] if email.blank?
     end
-    user_authentications.build(provider: omniauth['provider'], uid: omniauth['uid'])
+
+    if omniauth.fetch('info', {})['name'].present?
+      fname, lname = omniauth['info']['name'].split(' ')
+      self.first_name = fname if first_name.blank?
+      self.last_name = lname if last_name.blank? && lname.present?
+    end
+
+    # TODO: get user birthday
+
+    user_authentications.find_or_initialize_by(provider: omniauth['provider'], uid: omniauth['uid'])
   end
 
   def password_required?
