771e7b847b8704aed4d9acabf4359170dec5aff8
Elia Schito <elia@schito.me>
2020-01-02 12:12:55 +0100

    Use SolidusSupport::EngineExtensions::Decorators to manage decorators

--- lib/solidus_social/engine.rb
+++ lib/solidus_social/engine.rb
@@ -2,6 +2,8 @@
 
 module SolidusSocial
   class Engine < Rails::Engine
+    include SolidusSupport::EngineExtensions::Decorators
+
     engine_name 'solidus_social'
 
     config.autoload_paths += %W(#{config.root}/lib)
@@ -19,14 +21,6 @@ module SolidusSocial
         load File.join(__dir__, '../../app/models/spree/user_decorator.rb')
       end
     end
-
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
-    end
-
-    config.to_prepare(&method(:activate).to_proc)
   end
 
   def self.configured_providers
--- solidus_social.gemspec
+++ solidus_social.gemspec
@@ -33,7 +33,7 @@ Gem::Specification.new do |s|
   s.add_runtime_dependency 'omniauth-twitter'
   s.add_runtime_dependency 'solidus_auth_devise'
   s.add_runtime_dependency 'solidus_core', ['>= 1.0', '< 3']
-  s.add_runtime_dependency 'solidus_support'
+  s.add_runtime_dependency 'solidus_support', '~> 0.4.0'
 
   s.add_development_dependency 'solidus_dev_support'
 end
