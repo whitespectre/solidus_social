1f416b00191e2f8b33dd27bfaef45abbf6449346
Elia Schito <elia@schito.me>
2020-01-02 11:27:28 +0100

    Use the app passed to the initializer

--- lib/solidus_social/engine.rb
+++ lib/solidus_social/engine.rb
@@ -10,10 +10,10 @@ module SolidusSocial
       ::Spree::SocialConfig = ::Spree::SocialConfiguration.new
     end
 
-    initializer 'solidus_social.decorate_spree_user' do
-      next unless Rails.application.respond_to?(:reloader)
+    initializer 'solidus_social.decorate_spree_user' do |app|
+      next unless app.respond_to?(:reloader)
 
-      Rails.application.reloader.after_class_unload do
+      app.reloader.after_class_unload do
         # Reload and decorate the spree user class immediately after it is
         # unloaded so that it is available to devise when loading routes
         load File.join(__dir__, '../../app/models/spree/user_decorator.rb')
