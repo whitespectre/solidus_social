80871475d76adc533d188caeec931465bad40890
Mattia Roccoberton <mat@blocknot.es>
2020-01-17 12:42:23 +0100

    Update extension structure using dev_support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Configure Rails Environment
 ENV['RAILS_ENV'] ||= 'test'
 
 require 'solidus_dev_support/rspec/coverage'
@@ -8,4 +9,11 @@ require File.expand_path('dummy/config/environment.rb', __dir__)
 
 require 'solidus_dev_support/rspec/feature_helper'
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require f }
+
+RSpec.configure do |config|
+  config.infer_spec_type_from_file_location!
+  config.use_transactional_fixtures = false
+end
