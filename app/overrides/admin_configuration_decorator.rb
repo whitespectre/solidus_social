b004fecc243a95313427bcf4408ff3aa3569eb60
Memo Toro <memo.toro@gmail.com>
2020-04-26 18:53:24 +0100

    Spree::PermissionSets for AuthenticationMethod

--- app/overrides/admin_configuration_decorator.rb
+++ app/overrides/admin_configuration_decorator.rb
@@ -3,5 +3,10 @@
 Deface::Override.new(virtual_path: 'spree/admin/shared/_configuration_menu',
                      name: 'add_social_providers_link_configuration_menu',
                      insert_bottom: '[data-hook="admin_configurations_sidebar_menu"]',
-                     text: '<%= configurations_sidebar_menu_item I18n.t("spree.social_authentication_methods"), spree.admin_authentication_methods_path %>',
-                     disabled: false)
+                     disabled: false) do
+                       <<-HTML
+                        <% if can? :admin, Spree::AuthenticationMethod %>
+                          <%= configurations_sidebar_menu_item I18n.t("spree.social_authentication_methods"), spree.admin_authentication_methods_path %>
+                        <% end %>
+                        HTML
+                     end
