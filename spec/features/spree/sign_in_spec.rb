be8cc5cdb57c1155ccceadf68fb9acfceb1b2fea
piyushswain <piyush.swain3@gmail.com>
2022-08-17 16:57:48 +0530

    Add Tests for Github and Google Oauth 2 providers

--- spec/features/spree/sign_in_spec.rb
+++ spec/features/spree/sign_in_spec.rb
@@ -52,4 +52,108 @@ RSpec.describe 'Signing in using Omniauth', :js do
       expect(page).not_to have_selector 'div#social-signin-links'
     end
   end
+
+  context 'github' do
+    before do
+      Spree::AuthenticationMethod.create!(
+        provider: 'github',
+        api_key: 'fake',
+        api_secret: 'fake',
+        environment: Rails.env,
+        active: true
+      )
+      OmniAuth.config.mock_auth[:github] = {
+        'provider' => 'github',
+        'uid' => '123545',
+        'info' => {
+          'name' => 'mockuser',
+          'email' => 'mockuser@example.com',
+          'image' => 'mock_user_thumbnail_url'
+        },
+        'credentials' => {
+          'token' => 'mock_token',
+          'secret' => 'mock_secret'
+        }
+      }
+    end
+
+    it 'going to sign in' do
+      visit spree.login_path
+      click_on 'Login with github'
+      expect(page).to have_text 'You are now signed in with your github account.'
+      click_link 'Logout'
+      click_link 'Login'
+      click_on 'Login with github'
+      expect(page).to have_text 'You are now signed in with your github account.'
+    end
+
+    # Regression test for #91
+    it "attempting to view 'My Account' works" do
+      visit spree.login_path
+      click_on 'Login with github'
+      expect(page).to have_text 'You are now signed in with your github account.'
+      click_link 'My Account'
+      expect(page).to have_text 'My Account'
+    end
+
+    it "view 'My Account'" do
+      visit spree.login_path
+      click_on 'Login with github'
+      expect(page).to have_text 'You are now signed in with your github account.'
+      click_link 'My Account'
+      expect(page).not_to have_selector 'div#social-signin-links'
+    end
+  end
+
+  context 'google_oauth2' do
+    before do
+      Spree::AuthenticationMethod.create!(
+        provider: 'google_oauth2',
+        api_key: 'fake',
+        api_secret: 'fake',
+        environment: Rails.env,
+        active: true
+      )
+      OmniAuth.config.mock_auth[:google_oauth2] = {
+        'provider' => 'google_oauth2',
+        'uid' => '123545',
+        'info' => {
+          'name' => 'mockuser',
+          'email' => 'mockuser@example.com',
+          'image' => 'mock_user_thumbnail_url'
+        },
+        'credentials' => {
+          'token' => 'mock_token',
+          'secret' => 'mock_secret'
+        }
+      }
+    end
+
+    it 'going to sign in' do
+      visit spree.login_path
+      click_on 'Login with google_oauth2'
+      expect(page).to have_text 'You are now signed in with your google_oauth2 account.'
+      click_link 'Logout'
+      click_link 'Login'
+      click_on 'Login with google_oauth2'
+      expect(page).to have_text 'You are now signed in with your google_oauth2 account.'
+    end
+
+    # Regression test for #91
+    it "attempting to view 'My Account' works" do
+      visit spree.login_path
+      click_on 'Login with google_oauth2'
+      expect(page).to have_text 'You are now signed in with your google_oauth2 account.'
+      click_link 'My Account'
+      expect(page).to have_text 'My Account'
+    end
+
+    it "view 'My Account'" do
+      visit spree.login_path
+      click_on 'Login with google_oauth2'
+      expect(page).to have_text 'You are now signed in with your google_oauth2 account.'
+      click_link 'My Account'
+      expect(page).not_to have_selector 'div#social-signin-links'
+    end
+  end
 end
